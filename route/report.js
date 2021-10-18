b16e3701c91d83077f6bbe8f7f866f57c9c1bdbb
Thibaut <thibaut2001>
2021-10-18 15:24:50 +0200

    Report controller and router

--- route/report.js
+++ route/report.js
@@ -1,9 +1,9 @@
-//const ReportController = require("../controleur/report");
+const ReportController = require("../controller/report");
 const router = require("express").Router();
 
-/*router.get('/:id', ProduitControleur.getProduit);
-router.post('/', ProduitControleur.postProduit);
-router.patch('/', ProduitControleur.updateProduit);
-router.delete('/', ProduitControleur.deleteProduit);*/
+router.get('/:id', ReportController.getReport);
+router.post('/', ReportController.postReport);
+router.patch('/', ReportController.patchReport);
+router.delete('/', ReportController.deleteReport);
 
 module.exports = router;
