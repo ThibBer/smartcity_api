aee2df6a79cfa6be1c8ec4b677fdb4102ab2058e
Thibaut <thibaut2001>
2021-10-18 12:49:22 +0200

    Init project

---
+++ route/index.js
@@ -0,0 +1,10 @@
+/*Routers*/
+const UserRouter = require('./user');
+const ReportRouter = require('./report');
+
+const router = require("express").Router();
+
+router.use("/user", UserRouter);
+router.use("/report", ReportRouter);
+
+module.exports = router;
---
+++ route/user.js
@@ -0,0 +1,9 @@
+//const UserController = require("../controleur/user");
+const router = require("express").Router();
+
+/*router.get('/:id', ProduitControleur.getProduit);
+router.post('/', ProduitControleur.postProduit);
+router.patch('/', ProduitControleur.updateProduit);
+router.delete('/', ProduitControleur.deleteProduit);*/
+
+module.exports = router;
