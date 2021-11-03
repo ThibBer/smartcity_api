520e56836d91da93a9dc0254c7ececb555c24fb6
ThibautBerg <etu43163@henallux.be>
2021-11-03 19:01:00 +0100

    Update table name

--- model/v1/user.js
+++ model/v1/user.js
@@ -1,15 +1,15 @@
 module.exports.get = async (client, id) => {
-    return await client.query(`SELECT * FROM user WHERE id = $1`, [id]);
+    return await client.query(`SELECT TO_CHAR(birth_date::DATE, 'dd/mm/yyyy') as foramtted_birth_date, * FROM BackOfficeUser WHERE id = $1`, [id]);
 }
 
 module.exports.all = async (client) => {
-    return await client.query(`SELECT * FROM user`, []);
+    return await client.query(`SELECT TO_CHAR(birth_date::DATE, 'dd/mm/yyyy') as foramtted_birth_date, * FROM BackOfficeUser`, []);
 }
 
-module.exports.post = async (client, id, email, password, firstName, lastName, birthDate, role, city, street, zipCode, houseNumber) => {
-    return await client.query(`INSERT INTO user (id, email, password, first_name, last_name, birth_date, role, city, street, zip_code, house_number) VALUES 
-($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)`,
-        [id, email, password, firstName, lastName, birthDate, role, city, street, zipCode, houseNumber]);
+module.exports.post = async (client, email, password, firstName, lastName, birthDate, role, city, street, zipCode, houseNumber) => {
+    return await client.query(`INSERT INTO BackOfficeUser (id, email, password, first_name, last_name, birth_date, role, city, street, zip_code, house_number) VALUES 
+($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)`,
+        [email, password, firstName, lastName, birthDate, role, city, street, zipCode, houseNumber]);
 }
 
 module.exports.patch = async (client, id) => {
