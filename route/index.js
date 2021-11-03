857a4e7b61fd750a2136e9a08e887f65bacdc3e8
ThibautBerg <etu43163@henallux.be>
2021-11-03 19:01:32 +0100

    Add headers

--- route/index.js
+++ route/index.js
@@ -1,6 +1,12 @@
 const router = require("express").Router();
 const v1Router = require("./v1");
 
+//TODO Peut-on garder ça ?
+router.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 router.use("/v1", v1Router);
 
 module.exports = router;
