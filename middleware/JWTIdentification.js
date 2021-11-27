3dec69c3704a9442703de33b48dcf34bb3a08955
ThibautBerg <etu43163@henallux.be>
2021-11-27 18:34:22 +0100

    JWT Identification

---
+++ middleware/JWTIdentification.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const process = require('process');
+const jwt = require('jsonwebtoken');
+
+module.exports.identification = async (req, res, next) => {
+    const headerAuth = req.get('authorization');
+
+    if(headerAuth !== undefined && headerAuth.includes("Bearer")){
+        const jwtToken =  headerAuth.split(' ')[1];
+
+        try{
+            const decodedJwtToken = jwt.verify(jwtToken, process.env.SECRET_TOKEN);
+            req.session = decodedJwtToken;
+            req.session.authLevel = decodedJwtToken.role;
+
+            next();
+        }
+        catch (e) {
+            console.error(e);
+            res.sendStatus(400);
+        }
+    } else {
+        res.sendStatus(401);
+    }
+};
