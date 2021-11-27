c777564eb382a659c46df0a120c6d089f7156683
Nicolasbernard2000 <nicobern.play@gmail.com>
2021-11-27 10:37:30 +0100

    Set time zone

--- scripts/SQL/fillDB.sql
+++ scripts/SQL/fillDB.sql
@@ -1,3 +1,7 @@
+START TRANSACTION;
+SET TIME ZONE 'UTC-2';
+COMMIT;
+
 INSERT INTO BackOfficeUser (email, password, first_name, last_name, birth_date, role, city, street, zip_code, house_number)VALUES('thibaut.berg@hotmail.com', '$2b$10$OoWWML05uIJCHV4FlE7z7eLq3MRatgeiSIpBDumGM./zlRrynkaIm', 'Thibaut', 'Berg', '2001-10-28', 'admin', 'Marche-en-Famenne', 'Rue des Champs', 6900, 53);
 INSERT INTO BackOfficeUser (email, password, first_name, last_name, birth_date, role, city, street, zip_code, house_number)VALUES('nicolas.bernard@gmail.com', '$2b$10$dJIBLEEXU1n28Us5xtwCN.CJfMFA6AfpjvJTNE2wown7LRMdb4pvu', 'Nicolas', 'Bernard', '1995-01-01', 'admin', 'Spontin', 'Rue des Nicolas', 5581, 23);
 
