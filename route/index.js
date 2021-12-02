4803970f73087d2d23a55b1a23b1accbdbd64516
Thibaut <thibaut2001>
2021-12-02 10:28:50 +0100

    Remove todo

--- route/index.js
+++ route/index.js
@@ -1,13 +1,13 @@
 const router = require("express").Router();
 const v1Router = require("./v1");
 
-//TODO Peut-on garder ça ?
 router.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
     next();
 });
+
 router.use("/v1", v1Router);
 
 module.exports = router;
