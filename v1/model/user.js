a64b9735a83c10c953fde8377d793dd3f6fabfea
ThibautBerg <etu43163@henallux.be>
2021-12-19 14:46:48 +0100

    Fix User table name in queries

--- v1/model/user.js
+++ v1/model/user.js
@@ -1,5 +1,5 @@
 async function get(client, id) {
-    const {rows: users} = await client.query(`SELECT * FROM User WHERE id = $1`, [id]);
+    const {rows: users} = await client.query(`SELECT * FROM \"User\" WHERE id = $1`, [id]);
 
     return users[0];
 }
@@ -7,7 +7,7 @@ async function get(client, id) {
 module.exports.get = get;
 
 module.exports.getWithEmail = async (client, email) => {
-    const {rows: users} = await client.query('SELECT * FROM User WHERE email = $1', [email])
+    const {rows: users} = await client.query('SELECT * FROM \"User\" WHERE email = $1', [email])
     return users[0];
 }
 
@@ -16,23 +16,23 @@ module.exports.exist = async (client, id) => {
 }
 
 module.exports.filter = async (client, filter) => {
-    return await client.query("SELECT id, email, first_name, last_name, birth_date, role, city, street, zip_code, house_number FROM User WHERE id::varchar(11) ~ $1 OR email ~ $1 OR first_name ~ $1 OR last_name ~ $1 OR birth_date::varchar(50) ~ $1 OR role ~ $1 OR city ~ $1 OR street ~ $1 OR zip_code::varchar(11) ~ $1 OR house_number::varchar(11) ~ $1 ORDER BY id", [filter]);
+    return await client.query("SELECT id, email, first_name, last_name, birth_date, role, city, street, zip_code, house_number FROM \"User\" WHERE id::varchar(11) ~ $1 OR email ~ $1 OR first_name ~ $1 OR last_name ~ $1 OR birth_date::varchar(50) ~ $1 OR role ~ $1 OR city ~ $1 OR street ~ $1 OR zip_code::varchar(11) ~ $1 OR house_number::varchar(11) ~ $1 ORDER BY id", [filter]);
 }
 
 module.exports.filterWithOffsetLimit = async (client, filter, offset, limit) => {
     if(filter !== undefined){
-        return await client.query("SELECT id, email, first_name, last_name, birth_date, role, city, street, zip_code, house_number FROM User WHERE id::varchar(11) ~ $1 OR email ~ $1 OR first_name ~ $1 OR last_name ~ $1 OR birth_date::varchar(50) ~ $1 OR role ~ $1 OR city ~ $1 OR street ~ $1 OR zip_code::varchar(11) ~ $1 OR house_number::varchar(11) ~ $1 ORDER BY id OFFSET $2 LIMIT $3", [filter, offset, limit])
+        return await client.query("SELECT id, email, first_name, last_name, birth_date, role, city, street, zip_code, house_number FROM \"User\" WHERE id::varchar(11) ~ $1 OR email ~ $1 OR first_name ~ $1 OR last_name ~ $1 OR birth_date::varchar(50) ~ $1 OR role ~ $1 OR city ~ $1 OR street ~ $1 OR zip_code::varchar(11) ~ $1 OR house_number::varchar(11) ~ $1 ORDER BY id OFFSET $2 LIMIT $3", [filter, offset, limit])
     }
 
-    return await client.query("SELECT id, email, first_name, last_name, birth_date, role, city, street, zip_code, house_number FROM User ORDER BY id OFFSET $1 LIMIT $2", [offset, limit]);
+    return await client.query("SELECT id, email, first_name, last_name, birth_date, role, city, street, zip_code, house_number FROM \"User\" ORDER BY id OFFSET $1 LIMIT $2", [offset, limit]);
 }
 
 module.exports.countWithFilter = async (client, filter) => {
     if(filter !== undefined){
-        return await client.query("SELECT COUNT(*) as count FROM User WHERE id::varchar(11) ~ $1 OR email ~ $1 OR first_name ~ $1 OR last_name ~ $1 OR birth_date::varchar(50) ~ $1 OR role ~ $1 OR city ~ $1 OR street ~ $1 OR zip_code::varchar(11) ~ $1 OR house_number::varchar(11) ~ $1", [filter])
+        return await client.query("SELECT COUNT(*) as count FROM \"User\" WHERE id::varchar(11) ~ $1 OR email ~ $1 OR first_name ~ $1 OR last_name ~ $1 OR birth_date::varchar(50) ~ $1 OR role ~ $1 OR city ~ $1 OR street ~ $1 OR zip_code::varchar(11) ~ $1 OR house_number::varchar(11) ~ $1", [filter])
     }
 
-    return await client.query("SELECT COUNT(*) as count FROM User", []);
+    return await client.query("SELECT COUNT(*) as count FROM \"User\"", []);
 }
 
 module.exports.post = async (client, email, password, firstName, lastName, birthDate, role, city, street, zipCode, houseNumber) => {
@@ -104,5 +104,5 @@ module.exports.patch = async (client, id, email, password, firstName, lastName, 
 }
 
 module.exports.delete = async (client, id) => {
-    return await client.query('DELETE FROM User WHERE id = $1', [id]);
+    return await client.query('DELETE FROM \"User\" WHERE id = $1', [id]);
 }
